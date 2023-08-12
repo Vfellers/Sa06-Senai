06e55d7e7df2284863db45cb78ee674e35d43afc
Victor Feller <victor.feller@hotmail.com>
2023-08-12 12:04:16 -0300

    Frontend add

---
+++ appwebsenai/src/main/resources/static/script.js
@@ -0,0 +1,39 @@
+
+
+const formPessoa = document.getElementById("cadastroPessoa"); //id do formulario
+
+const tabelaPessoa = document.getElementById("tabelaPessoas")
+    .getElementsByTagName('tbody')[0];
+
+const btnAll = document.getElementById("listarPessoas");
+
+btnAll.addEventListener("click", function (){
+    fetch("/all")
+        .then(response => response.json())
+        .then(data => {
+
+            tabelaPessoa.innerHTML="";
+            data.forEach(pessoa => {
+                let row = tabelaPessoa.insertRow();
+                row.insertCell(0).textContent = pessoa.id;
+                row.insertCell(1).textContent = pessoa.name;
+                row.insertCell(2).textContent = pessoa.sexo;
+            })
+        })
+});
+
+
+
+formPessoa.addEventListener("submit", function (event){
+    event.preventDefault(); // permite customizar o evento no submit
+    let formDados= new FormData(formPessoa);
+    let parametros= new URLSearchParams(formDados);
+
+    fetch("/person?"+ parametros.toString(),{                       //chamar os dados like postman
+        method: "POST"
+    }).then(response => response.json())
+        .then(data => {
+            document.getElementById("id").value = data.id;
+        })
+});
+
